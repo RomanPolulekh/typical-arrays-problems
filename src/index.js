de145115f10ef3c4a70a1c3d84ce461883f75718
RomanPolulekh <70841623+RomanPolulekh@users.noreply.github.com>
2020-10-03 01:45:56 +0300

    feat: add code for funcs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
+  if(array===null||array===undefined)
   return 0;
+  let min=0;
+  for(let i=0;i<array.length;i++ )
+  {
+let buff=array[i];
+if(buff<min) min=buff;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
+  if(array===null||array===undefined)
   return 0;
+  let max=0;
+  for(let i=0;i<array.length;i++ )
+  {
+let buff=array[i];
+if(buff>max) max=buff;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if(!(Array.isArray(array) && array.length > 0)) return 0;
+  let sum=0;
+  for(let i=0;i<array.length;i++ )
+  {
+sum=sum+array[i];
+  }
+  return sum/array.length;
 }
